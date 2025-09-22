c7a47396fdcdce09a59bf3593caf66e4999fe12d
mayur1212 <takkemayur456@gmail.com>
2025-09-22 15:38:18 +0530

    new chnges isuue solbve dashboard resfresh

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -6,10 +7,10 @@ import PublicLayout from './layouts/PublicLayout';
 import DashboardLayout from './layouts/DashboardLayout';
 
 // Pages
-import Home from './pages/Home/home';
+import Home from './pages/Home/Home';
 import RegisterForm from './components/RegisterForm/RegisterForm';
 import Navbar from './components/navbar/Navbar';
-import Login from './components/login/login'; // renamed to PascalCase for consistency
+import Login from './components/login/login';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import ResetPassword from './pages/ResetPassword/ResetPassword';
 import DashboardHome from './pages/dashboard/DashboardHome';
@@ -17,15 +18,12 @@ import Events from './pages/dashboard/Events';
 import EventManagement from './pages/dashboard/EventManagement';
 import Profile from './pages/dashboard/Profile';
 
-
-
 // Auth & Context
 import ProtectedRoute from './components/ProtectedRoute';
 import { UserProvider } from './context/UserContext';
 
 function App() {
   return (
-
     <UserProvider>
       <BrowserRouter>
         <Routes>
@@ -42,7 +40,11 @@ function App() {
           {/* Protected Dashboard Routes */}
           <Route
             path="/dashboard"
-            element={<ProtectedRoute><DashboardLayout /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <DashboardLayout />
+              </ProtectedRoute>
+            }
           >
             <Route index element={<DashboardHome />} />
             <Route path="events" element={<Events />} />
@@ -50,6 +52,10 @@ function App() {
             <Route path="profile" element={<Profile />} />
           </Route>
 
+          {/* Fallback route for SPA */}
+          <Route path="*" element={<PublicLayout />}>
+            <Route index element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </UserProvider>
